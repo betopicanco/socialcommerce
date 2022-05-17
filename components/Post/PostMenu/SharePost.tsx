5f1d08ea84d00f0d6fd94c93c1ab5d22e1802d5c
betopicanco <betopicanco@gmail.com>
2022-05-16 23:47:50 -0300

    Update SharePost.tsx

--- components/Post/PostMenu/SharePost.tsx
+++ components/Post/PostMenu/SharePost.tsx
@@ -1,11 +1,25 @@
+import { useContext } from "react";
+import PostContext from "../../../Context/PostProvider";
 import Share from "../../Icons/Share";
 
+
 const SharePost = () => {
+  const { id, title } = useContext(PostContext);
+  const share = () => {
+    if(navigator.share !== undefined) {
+      navigator.share({
+        title: title,
+        text: 'Compartilhando o post',
+        url: `https://socialcommerce.vercel.app/post/${id}`
+      }).catch((error) => console.log('Error sharing', error));
+    }
+  }
+
   return (
-    <div>
+    <div onClick={() => share()}>
       <Share />
     </div>
   )
 };
 
-export default SharePost;
+export default SharePost; 
